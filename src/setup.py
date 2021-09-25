58cf304caec8bfaff8efa06fa5850a60123719da
ALOY\micro <ernest@czerwonka.de>
2021-09-25 08:07:50 +0200

    Initial commit of setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mqtt2json",
+    version="0.1.0",
+    description="Extract tracks from an mkv and add them to another mkv with an m4v Video",
+    author="cze",
+    author_email="ernest@czerwonka.de",
+    packages=find_packages(),
+    install_requires=["Flask<=2", "prometheus-flask-exporter", "paho-mqtt", "gunicorn", "pyyaml"],
+)
